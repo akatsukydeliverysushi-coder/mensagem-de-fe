046302f3d28341d9defdd3679973b40d486ee071
akatsukydeliverysushi-coder <akatsuky.deliverysushi@gmail.com>
2026-09-03 10:42:59 -0300

    Atualizar cache para carregar o novo fundo

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="mensagem-fe-v3";
-const FILES=["./","./index.html","./style.css?v=3","./app.js?v=3","./manifest.json?v=3","./fundo-mensagem-fe-app.webp"];
-self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});
+const CACHE="mensagem-fe-v2";
+const FILES=["./","./index.html","./style.css","./app.js","./manifest.json","./fundo-exato.jpg"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
